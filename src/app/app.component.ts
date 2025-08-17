a022bba8c446f8bc6b75e83c74e0b77170b4ad16
KailanySousa <kailany-sousa2010@hotmail.com>
2025-08-17 20:27:04 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, HomeComponent],
-  template: '<app-header></app-header><app-home></app-home>',
+    selector: 'app-root',
+    imports: [HeaderComponent, HomeComponent],
+    template: '<app-header></app-header><app-home></app-home>'
 })
 export class AppComponent {}
