93f40a0ba9cd585db9b8adc457a2c8834490c29f
KailanySousa <kailany-sousa2010@hotmail.com>
2025-10-11 23:01:48 -0300

    .

--- src/app/features/capitulos/lista/lista.component.ts
+++ src/app/features/capitulos/lista/lista.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input, output } from '@angular/core';
+import { Component, computed, inject, input, output } from '@angular/core';
 import { ICapitulo } from '../../../shared/interfaces/capitulo.interface';
 import { CommonModule } from '@angular/common';
 import { CapituloStore } from '../../../shared/stores/capitulo.store';
+import { LeituraStore } from '../../../shared/stores/leitura.store';
 
 @Component({
   selector: 'app-lista',
@@ -9,10 +10,17 @@ import { CapituloStore } from '../../../shared/stores/capitulo.store';
   templateUrl: './lista.component.html',
 })
 export class ListaComponent {
+  livroId = input.required<number>();
   capitulos = input.required<ICapitulo[]>();
   editarCapitulo = output<ICapitulo>();
 
   #store = inject(CapituloStore);
+  #leituraStore = inject(LeituraStore);
+
+  leituraFinalizada = computed(() => {
+    const leitura = this.#leituraStore.item(this.livroId());
+    return leitura && leitura.status === 'finalizado';
+  });
 
   editar(c: ICapitulo) {
     this.editarCapitulo.emit(c);
@@ -20,5 +28,10 @@ export class ListaComponent {
 
   excluir(id: number) {
     this.#store.remove(id);
+  }
+
+  marcar(capitulo: ICapitulo) {
+    if (this.leituraFinalizada()) return;
+    this.#store.update(capitulo.id, { concluido: !capitulo.concluido });
   }
 }
--- src/app/features/livro/capitulos-livro/capitulos-livro.component.ts
+++ src/app/features/livro/capitulos-livro/capitulos-livro.component.ts
@@ -1,6 +1,8 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { ICapitulo } from '../../../shared/interfaces/capitulo.interface';
 import { RouterModule } from '@angular/router';
+import { CapituloStore } from '../../../shared/stores/capitulo.store';
+import { LeituraStore } from '../../../shared/stores/leitura.store';
 
 @Component({
   selector: 'app-capitulos-livro',
@@ -8,9 +10,23 @@ import { RouterModule } from '@angular/router';
   templateUrl: './capitulos-livro.component.html',
 })
 export class CapitulosLivroComponent {
+  livroId = input.required<number>();
   capitulos = input.required<ICapitulo[]>();
+
+  #store = inject(CapituloStore);
+  #leituraStore = inject(LeituraStore);
+
+  leituraFinalizada = computed(() => {
+    const leitura = this.#leituraStore.item(this.livroId());
+    return leitura && leitura.status === 'finalizado';
+  });
 
   qtdCapitulos = computed(
     () => this.capitulos().filter((c) => c.concluido).length
   );
+
+  marcar(capitulo: ICapitulo) {
+    if (this.leituraFinalizada()) return;
+    this.#store.update(capitulo.id, { concluido: !capitulo.concluido });
+  }
 }
