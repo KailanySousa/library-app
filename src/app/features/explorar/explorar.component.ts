b4bcbd2c83039b25aadcd68c5b825d1504195714
KailanySousa <kailany-sousa2010@hotmail.com>
2025-10-19 15:43:43 -0300

    .

--- src/app/features/explorar/explorar.component.ts
+++ src/app/features/explorar/explorar.component.ts
@@ -1,16 +1,17 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   // computed,
   inject,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header.component';
-import { EStatus } from '../../shared/enums/status.enum';
 import { CardLivroComponent } from './card-livro/card-livro.component';
 import { ListaVaziaComponent } from '../../shared/components/lista-vazia/lista-vazia.component';
 import { LivroStore } from '../../shared/stores/livro.store';
+import { LeituraStore } from '../../shared/stores/leitura.store';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,10 +27,39 @@ import { LivroStore } from '../../shared/stores/livro.store';
   templateUrl: './explorar.component.html',
 })
 export class ExplorarComponent {
-  readonly EStatus = EStatus;
   #livroStore = inject(LivroStore);
+  #leituraStore = inject(LeituraStore);
 
-  //lidos = computed(() => this.#livroStore.by('status', EStatus.LIDO));
-  //desejos = computed(() => this.#livroStore.by('status', EStatus.DESEJO));
-  //lendo = computed(() => this.#livroStore.by('status', EStatus.LENDO));
+  lendo = computed(() => {
+    const leiturasAndamento = this.#leituraStore
+      .by('status', 'lendo')
+      .flatMap((l) => (l.status === 'lendo' ? l.livroId : null), 'livroId')
+      .filter((l) => l !== null);
+
+    return leiturasAndamento
+      .map((livroId) => this.#livroStore.item(livroId))
+      .slice(0, 3);
+  });
+  lidos = computed(() => {
+    const leiturasFinalizadas = this.#leituraStore
+      .by('status', 'finalizado')
+      .flatMap((l) => (l.status === 'finalizado' ? l.livroId : null), 'livroId')
+      .filter((l) => l !== null);
+
+    return leiturasFinalizadas
+      .map((livroId) => this.#livroStore.item(livroId))
+      .slice(0, 3);
+  });
+
+  iniciar = computed(() => {
+    return this.#livroStore
+      .livros()
+      .map((livro) => {
+        const leitura = this.#leituraStore.itemBy('livroId', livro.id);
+        if (!leitura) return livro;
+        return null;
+      })
+      .filter((l) => l !== null)
+      .slice(0, 3);
+  });
 }
